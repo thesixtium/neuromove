5879ba92170d56ea75c0ef772a2c4b8feee73f2e
Philippa <100964249+Philippamadill@users.noreply.github.com>
2024-12-02 21:52:27 -0700

    Just a couple things

--- arrowtesting/p300Arrows.js
+++ arrowtesting/p300Arrows.js
@@ -1,33 +1,32 @@
-const _idoptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
-const _arrows = ["ca", "cb", "cc", "cd", "ce", "cf", "cg", "ch", "ci"];
+const _idoptions = [/*"a",*/ "b", /*"c", */"d", "e", "f"/*, "g", "h", "i"*/];
+const _arrows = [/*"ca",*/ "cb", /*"cc",*/ "cd", "ce", "cf"/*, "cg", "ch", "ci"*/];
 const _sequence1 = [];
 const _sequence2 = [];
 const _sequence3 = [];
 
-
-const l45 = document.getElementById("ca");
+const start_time = Date.now();
+/*const l45 = document.getElementById("ca");
 l45.width = 100;
 l45.height = 100;
-const cl45 = l45.getContext("2d");
+const cl45 = l45.getContext("2d");*/
 const fwd = document.getElementById("cb");
 const cfwd = fwd.getContext("2d");
-const r45 = document.getElementById("cc");
-const cr45 = r45.getContext("2d");
+//const r45 = document.getElementById("cc");
+//const cr45 = r45.getContext("2d");
 const l = document.getElementById("cd");
 const cl = l.getContext("2d");
 const stop = document.getElementById("ce");
 const cstop = stop.getContext("2d");
 const r = document.getElementById("cf");
 const cr = r.getContext("2d");
-const l135 = document.getElementById("cg");
+/*const l135 = document.getElementById("cg");
 const cl135 = l135.getContext("2d");
 const bwd = document.getElementById("ch");
 const cbwd = bwd.getContext("2d");
 const r135 = document.getElementById("ci");
-const cr135 = r135.getContext("2d");
+const cr135 = r135.getContext("2d");*/
 
-const _canvases = [cl45, cfwd, cr45, cl, cstop, cr, cl135, cbwd, cr135];
-
+const _canvases = [/*cl45,*/ cfwd, /*cr45, */cl, cstop ,cr, /*cl135, cbwd, cr135*/];
 const defaultColour = "white";
 function pickSequence(array, array2){
     let temp = [];
@@ -48,38 +47,48 @@ function pickSequence(array, array2){
             i++;
         }
     }
+    
+    array.unshift(array[0]);
+    array2.unshift(array2[0]);
     console.log("array: " + array);
-    console.log("arrows: " + _sequence2);
+    console.log("arrows: " + array2);
 }
 function flashSequence(array, array2){
     //for(let j = 0; j < 3; j++){
         pickSequence(array, array2);
         //array2[0].fillStyle = "black";
         //array2[0].fill()
-    let i = 0;
+        console.log("array at flash: " + array);
+        let i = 0;
     let interval = setInterval(function(){
-        if (i < _idoptions.length){
+        if (i <array2.length){
+            
+            console.log("start i: " + array[i]);
+            console.log(Date.now() - start_time);
             document.getElementById(array[i]).style.backgroundColor = "black";
             array2[i].fillStyle = "white";
             array2[i].fill();
             setTimeout(function(){
-                document.getElementById(array[i]).style.backgroundColor = defaultColour;
+                document.getElementById(array[i]).style.backgroundColor = "white";
                 array2[i].fillStyle = "black";
                 array2[i].fill();
             }, 200);
+            console.log("end i: " + array[i]);
+            console.log(Date.now() - start_time);
             i++;
           /*  if (array[i] == "ca"){
                 l45.width +=15;
                 drawArrows();
             }*/
         } else {
+            drawArrows();
             clearInterval(interval);
         }
-    }, 440);
+    }, 640);
 }
 
 function drawArrows(){
-    
+   /* 
     cl45.beginPath();
     cl45.moveTo(85, 100);
     cl45.lineTo(35, 60);
@@ -94,7 +103,8 @@ function drawArrows(){
     //_canvases[0].fillStyle = "pink";
     cl45.fillStyle = "white";
     cl45.fill();
-
+    document.getElementById("a").style.backgroundColor = "black";
+*/
     cfwd.beginPath();
     cfwd.moveTo(40, 95);
     cfwd.lineTo(40, 45);
@@ -103,12 +113,13 @@ function drawArrows(){
     cfwd.lineTo(90, 45);
     cfwd.lineTo(60, 45);
     cfwd.lineTo(60, 95);
-    cfwd.lineTo(40, 95);
+    cfwd.lineTo(35, 95);
     cfwd.lineWidth = 10;
     cfwd.stroke();
     cfwd.fillStyle = "white";
     cfwd.fill();
-
+    document.getElementById("b").style.backgroundColor = "black";
+/*
     cr45.beginPath();
     cr45.moveTo(15, 100);
     cr45.lineTo(65, 60);
@@ -121,6 +132,7 @@ function drawArrows(){
     cr45.stroke();
     cr45.fillStyle = "white";
     cr45.fill();
+    document.getElementById("c").style.backgroundColor = "black";
 
     cl135.beginPath();
     cl135.moveTo(85, 20);
@@ -160,7 +172,7 @@ function drawArrows(){
     cr135.lineWidth = 10;
     cr135.stroke();
     cr135.fillStyle = "white";
-    cr135.fill();
+    cr135.fill();*/
 
     cl.beginPath();
     cl.moveTo(90, 70);
@@ -170,11 +182,12 @@ function drawArrows(){
     cl.lineTo(40, 30);
     cl.lineTo(40, 50);
     cl.lineTo(90, 50);
-    cl.lineTo(90, 70);
+    cl.lineTo(90, 75);
     cl.lineWidth = 10;
     cl.stroke();
     cl.fillStyle = "white";
     cl.fill();
+    document.getElementById("d").style.backgroundColor = "black";
 
     cr.beginPath();
     cr.moveTo(10, 70);
@@ -184,14 +197,15 @@ function drawArrows(){
     cr.lineTo(60, 30);
     cr.lineTo(60, 50);
     cr.lineTo(10, 50);
-    cr.lineTo(10, 70);
+    cr.lineTo(10, 75);
     cr.lineWidth = 10;
     cr.stroke();
     cr.fillStyle = "white";
     cr.fill();
+    document.getElementById("f").style.backgroundColor = "black";
 
     cstop.beginPath();
-    cstop.moveTo(35, 25);
+    cstop.moveTo(32, 25);
     cstop.lineTo(70, 25);
     cstop.lineTo(90, 45);
     cstop.lineTo(90, 75);
@@ -204,17 +218,21 @@ function drawArrows(){
     cstop.stroke();
     cstop.fillStyle = "white";
     cstop.fill();
+    document.getElementById("e").style.backgroundColor = "black";
 }
 function doTheThing(){
     
-
+//flashSequence(_sequence1, _sequence2);
    //flashSequence(_sequence1);
     //setTimeout(function(){
-    for(let i = 0; i<7; i++){
-    setTimeout(function(){flashSequence(_sequence1, _sequence2);}, (3600*i));}//}, 2500);
+    console.log("block start: " + Date.now()-start_time);
+    for(let i = 0; i<2; i++){
+    setTimeout(function(){flashSequence(_sequence1, _sequence2); /*console.log("cycle "+ i+ " start: " + Date.now()-start_time);*/}, (2650*i));}//}, 2500);*/
+    console.log("cycle " + i + "end: " + Date.now()-start_time);
     //flashSequence(_sequence3);
     
 }
+//console.log("block end: " + Date.now()-start_time);
 //}
 addEventListener('DOMContentLoaded', drawArrows());
 addEventListener('DOMContentLoaded', doTheThing());
