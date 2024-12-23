0d8e57477a481c125ec738c16fc9451a18f0eb3d
Dani Jourdain <danijourdain@icloud.com>
2024-12-23 15:31:59 -0700

    do not ignore data ;-;

--- IDUN_P300_test/xdf_to_csv.py
+++ IDUN_P300_test/xdf_to_csv.py
@@ -1,5 +1,5 @@
 import lsl_xdf_reader
 
-filepath = "C:/Users/danij/Documents/Capstone/neuromove/IDUN_P300_test/data/sub-P002/ses-S001/eeg/"
+filepath = "D:/DATA/xdf/"
 streams = lsl_xdf_reader.parse_xdf(filepath)
 print(streams)
