13f4bcc1c3f082de7b9e4e1a5a2557b6f944bb59
Dani Jourdain <danijourdain@icloud.com>
2025-02-07 10:10:14 -0700

    shared memory messenger implemented: NOT TESTED

--- BCI_Processing/src/bci_essentials_wrappers/output/shared_memory_messenger.py
+++ BCI_Processing/src/bci_essentials_wrappers/output/shared_memory_messenger.py
@@ -1,14 +1,37 @@
 from lib.bci_essentials.bci_essentials.io.messenger import Messenger
 
+from .....src.RaspberryPi.SharedMemory import SharedMemory
+
 class SharedMemoryMessenger(Messenger):
-    def __init__(self):
+    def __init__(self, debug: bool=False):
         super().__init__()
 
+        self.__write_to_text = debug
+        self.__shared_memory = SharedMemory("bci_selection", size=10, create=True)
+
+        if self.__write_to_text:
+            self.__filepath = "BCI_OUTPUT_DEBUG.txt"
+            open(self.__filepath, "w")
+
+    def __open_file(self):
+        file = open(self.__filepath, "a")
+
+        return file
+
     def ping(self):
-        return super().ping()
+        if self.__write_to_text:
+            file = self.__open_file()
+            file.write("ping\n")
     
     def marker_received(self, marker):
-        return super().marker_received(marker)
+        if self.__write_to_text:
+            file = self.__open_file()
+            file.write(f"processed marker: {marker}\n")
     
     def prediction(self, prediction):
-        return super().prediction(prediction)
+        if self.__write_to_text:
+            file = self.__open_file()
+            file.write(f"Predicted {prediction.labels} with confidence {prediction.probabilities}\n")
+
+        # write to shared memory
+        self.__shared_memory.write_string(f"Prediction: {prediction.labels} w/ {prediction.probabilities}")
