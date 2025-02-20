14447d16e294e10bf704e03be264b0b4d8763aaa
Aleks B <ajrberezowski@gmail.com>
2025-02-20 12:09:39 -0700

    refactor

--- src/Frontend/frontend.py
+++ src/Frontend/frontend.py
@@ -1,63 +1,16 @@
 import time
 import numpy as np
 from io import BytesIO
-from random import shuffle
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
-import streamlit as st
 from streamlit_extras.stylable_container import stylable_container
 
-from pylsl import StreamInfo, StreamOutlet, local_clock
+from pylsl import StreamInfo, StreamOutlet
 
-from src.RaspberryPi.States import States
 from src.RaspberryPi.SharedMemory import SharedMemory
 from src.Frontend.style import *
-
-def send_marker(number_of_options: int, flashed_as_num: int):
-    st.session_state["marker_outlet"].push_sample([f"p300,s,{number_of_options},{-1},{flashed_as_num}"], local_clock())
-
-
-def give_local_sequence_list():
-    all_buttons = ["up", "left", "right", "stop", "switch"]
-    return_list = []
-    list_appends = 0
-
-    while list_appends < 5:
-        shuffle(all_buttons)
-        if len(return_list) == 0 or return_list[-1] != all_buttons[0]:
-            return_list += all_buttons
-            list_appends += 1
-
-    return_list = [item for pair in zip(return_list, ["break"] * len(return_list)) for item in pair][:-1]
-    st.session_state["flash_sequence"] = return_list
-
-
-def give_map_sequence_list():
-    all_buttons = ["1", "2", "3", "4", "switch"]
-    return_list = []
-    list_appends = 0
-
-    while list_appends < 5:
-        shuffle(all_buttons)
-        if len(return_list) == 0 or return_list[-1] != all_buttons[0]:
-            return_list += all_buttons
-            list_appends += 1
-
-    return_list = [item for pair in zip(return_list, ["0"] * len(return_list)) for item in pair]
-    st.session_state["map_sequence"] = return_list
-
-def direction_update(direction):
-    st.session_state["local_driving_memory"].write_string(direction)
-
-
-def switch():
-    if st.session_state["state"] == States.LOCAL:
-        st.session_state["state"] = States.DESTINATION
-        st.session_state["requested_next_state_memory"].write_string("4")
-    else:
-        st.session_state["state"] = States.LOCAL
-        st.session_state["requested_next_state_memory"].write_string("3")
+from src.Frontend.frontend_methods import *
 
 with open("Frontend/frontend.css") as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
