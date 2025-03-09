bfb7fd974fa9d6828d6dd30a19bbb59a83e5df84
Dani Jourdain <danijourdain@icloud.com>
2025-03-09 17:06:13 -0600

    basic Panda comms done

--- src/LattePanda/client.py
+++ src/LattePanda/client.py
@@ -1,23 +1,85 @@
 import socket
+import subprocess
+import threading
+import time
 
-def start_client():
-    # Create a socket object
+RASPBERRY_PI_PORT = 12345
+DSI_PORT = "COM3"
+SERVER_IP_ADDR = "10.0.0.56"
+
+def init_socket():
+    # Create socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((SERVER_IP_ADDR, RASPBERRY_PI_PORT))
+ 
+    return client_socket
+
+def run_dsi_streamer():
+    # DSI Streamer (for headset impedance checking)
+    subprocess.run(["c:/Users/danij/Documents/Capstone/DSI Streamer 1.08.119/DSI-Streamer-v.1.08.119.exe"])
+    print("DSI Streamer closed")
+
+def run_dsi2lsl():
+    # DSI2LSL
+    subprocess.run(["c:/Users/danij/Documents/Capstone/dsi2lsl-win/dsi2lsl.exe", f'port={DSI_PORT}', 'lsl-stream-name=DSI7', 'montage=F4,C4,S1,S3,C3,F3'])
+
+def send_ping(socket: socket.socket, stop_event: threading.Event):
+    while not stop_event.set():
+        try:
+            socket.sendall(b'ping')
+            time.sleep(1)
+        except Exception as e:
+            print(f"Error sending ping: {e}")
+            break
+
+def listen_to_pi(socket: socket.socket, stop_event: threading.Event):
+    while not stop_event.set():
+        try:
+            data = socket.recv(1024).decode('ascii')
+            print(f"Received: {data}")
+            if "STOP" in data:
+                stop_event.set()
+                print("Stop signal received from Pi")
+        except Exception as e:
+            print(f"Error receiving data: {e}")
+            break
+
+def main():
+    socket = init_socket()
+    stop_event= threading.Event()
+
+    # ping Pi
+    ping_thread = threading.Thread(target=send_ping, args=(socket, stop_event,))
+    ping_thread.daemon = True
+    ping_thread.start()
+
+    # listen to Pi
+    listen_thread = threading.Thread(target=listen_to_pi, args=(socket, stop_event,))
+    listen_thread.daemon = True
+    listen_thread.start()
+
+    # start DSI Streamer
+    try:
+        run_dsi_streamer()
+    except Exception as e:
+        error_msg = f"DSI Streamer failed: {e}"
+        print(error_msg)
+        socket.send(error_msg.encode('ascii'))
+
+    # start DSI2LSL
+    try:
+        run_dsi2lsl()
+    except Exception as e:
+        error_msg = f"DSI2LSL failed: {e}"
+        print(error_msg)
+        socket.send(error_msg.encode('ascii'))
+
+    listen_thread.join()
+    print("shutting down...")
+
+    socket.close()
+
     
-    # Get local machine name
-    host = "10.0.0.237"
-    port = 12345
-    
-    # Connection to hostname on the port
-    client_socket.connect((host, port))
-    
-    # Receive no more than 1024 bytes
-    message = client_socket.recv(1024)
-    
-    print(message.decode('ascii'))
-    
-    # Close the connection
-    client_socket.close()
 
 if __name__ == "__main__":
-    start_client()
+    main()
