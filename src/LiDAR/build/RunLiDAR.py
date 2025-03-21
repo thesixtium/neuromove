455ec67ac81573a74337cdaa3c16c91fca0ba0e5
Aleks B <ajrberezowski@gmail.com>
2025-03-21 12:42:59 -0600

    Update RunLiDAR.py

--- src/LiDAR/build/RunLiDAR.py
+++ src/LiDAR/build/RunLiDAR.py
@@ -8,6 +8,9 @@ class RunLiDAR:
         self.serial_read_thread.start()
 
     def start(self):
+        print("cmake:")
         subprocess.run(["cmake src/LiDAR/build"], shell=True)
+        print("make:")
         subprocess.run(["make src/LiDAR/bin -j2"], shell=True)
+        print("run:")
         subprocess.run(["src/LiDAR/bin/aleks_lidar"], shell=True)
