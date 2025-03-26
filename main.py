733aa0ed2eb752d5644a7fde6ee3224c81a5584a
Aleks B <ajrberezowski@gmail.com>
2025-03-26 12:03:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,14 @@
 import time
 import logging
 import numpy as np
+import signal
+import sys
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 from src.RaspberryPi.Driving import Driving
 from src.RaspberryPi.InternalException import *
