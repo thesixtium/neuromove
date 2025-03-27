f52f13eebc4942712ccfa22378648fa358c50fd1
Aleks B <ajrberezowski@gmail.com>
2025-03-26 21:15:38 -0600

    Update frontend_methods.py

--- src/Frontend/frontend_methods.py
+++ src/Frontend/frontend_methods.py
@@ -89,7 +89,7 @@ def destination_driving_update(target_region, cropped_data, origin, point):
     #st.session_state["target_location"] = point
     print(f"Destination selected {target_region} with center {point}, doing nothing right now")
 
-    st.session_state["path"] = jps(cropped_data, origin[0], origin[1], point[0], point[1])
+    st.session_state["path"] = jps(cropped_data, origin[0], origin[1], point[1], point[0])
     print(f'D E S T   D R I V I N G: {st.session_state["path"]}')
     st.session_state["destination_driving_state"] = DestinationDrivingStates.TRANSLATE_TO_MOVEMENT
 
