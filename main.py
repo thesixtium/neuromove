147a6fa21f58f2a29dbeaff648644c4d299aa55c
Dani Jourdain <danijourdain@icloud.com>
2025-03-27 20:44:20 -0600

    moreeee memory

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def main():
                         point_selection_memory = SharedMemory(shem_name="point_selection", size=100000, create=True)
                         destination_driving_state_memory = SharedMemory(shem_name="destination_driving_state", size=10, create=True)
                         frontend_origin_memory = SharedMemory(shem_name="frontend_origin_memory", size=100, create=True)
-                        bci_selection_memory = SharedMemory(shem_name="bci_selection", size=20, create=True)
+                        bci_selection_memory = SharedMemory(shem_name="bci_selection", size=200, create=True)
                         print("Done")
 
                         frontend = RunUI()
