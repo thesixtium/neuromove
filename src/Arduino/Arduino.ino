e89dfc12f5145ff0772c23b0b5b13fea3601e488
Aleks B <ajrberezowski@gmail.com>
2025-04-08 23:33:13 -0600

    Arduino

--- src/Arduino/Arduino.ino
+++ src/Arduino/Arduino.ino
@@ -1,67 +1,22 @@
-#define scaling_m 0.00686
-#define scaling_b 0.589
-
-// Pins
-int pinArray[] = { 2, 3, 4, 5 };
-int fsrAnalogPin = 0;
-int sensorPins[] = { 8, 9, 10, 11, 12, 13 };
-
-// Readings
-int fsrReading;
-int drivingMode;
-int mode;
-int distance = 0;
-long duration = 0;
-
-void read_sensor( int sensorPin ) {
-  // TODO: Add in a moving average or something similar to smooth data
-  duration = pulseIn( sensorPin, HIGH );
-  distance = ( scaling_m * duration ) + scaling_b;
-
-  //Serial.print( "S" );
-  //Serial.print( sensorPin - sensorPins[0] );
-  //Serial.print( ":" );
-  //Serial.println( distance );
-}
-
+// Set all motors to off
 void stop() {
-  // Set all motors to off
   for (int i = 0; i < (sizeof(pinArray)/sizeof(int)); i++){
-    pinMode(pinArray[i],OUTPUT);
-    digitalWrite(pinArray[i], LOW);    // initialise all relays to HIGH (light off)
+    pinMode(pinArray[i], OUTPUT);
+    digitalWrite(pinArray[i], LOW);
   }
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-
-  // Set FSR to read
-  pinMode(fsrAnalogPin, INPUT);
-
-  // Set all ultrasonic sensors to read
-  for (int i = 0; i < (sizeof(sensorPins)/sizeof(int)); i++){
-    pinMode(sensorPins[i], INPUT);
-  }
-
-  mode = 9;
   stop();
 }
 
 void loop() {
-  // Writing FSR Data
-  //fsrReading = analogRead( fsrAnalogPin );
-  //Serial.print( "F1:" );
-  //Serial.println( fsrReading );
-
-  // Writing Ultrasonic Data
-  //for (int i = 0; i < (sizeof(sensorPins)/sizeof(int)); i++){
-  //  read_sensor( sensorPins[i] );
-  //}
+  int mode;
 
   // Reading Motor Control
   if (Serial.available() > 0) {
-    mode = Serial.read() -'0';
+    mode = Serial.read() - '0';
     stop();
     Serial.println(mode);
 
--- src/Arduino/ArduinoUno.py
+++ src/Arduino/ArduinoUno.py
@@ -1,7 +1,6 @@
 import serial
 from enum import Enum
-import threading
-from src.RaspberryPi.InternalException import SensorDistanceAlert, CouldNotOpenPort, ArduinoNotConnected
+from src.RaspberryPi.InternalException import CouldNotOpenPort
 import pyduinocli
 
 class MotorDirections(Enum):
@@ -11,87 +10,23 @@ class MotorDirections(Enum):
     RIGHT = b"d"
     STOP = b"x"
 
-class Sensors(Enum):
-    ULTRASONIC_1 = 1
-    ULTRASONIC_2 = 2
-    ULTRASONIC_3 = 3
-    ULTRASONIC_4 = 4
-    ULTRASONIC_5 = 5
-    ULTRASONIC_6 = 6
-    FORCE_SENSOR = 7
-
 class ArduinoUno:
-
-    def __init__(self, port='/dev/ttyACM0', baudrate=9600, timeout=1, ultrasonic_minimum_distance=10):
-        #print("\nFlashing board")
-        #arduino = pyduinocli.Arduino("./arduino-cli")
-        #print(f"\tArduino: {arduino}")
-        #brds = arduino.board.list()
-        #print(f"\tBoards: {brds}")
-
-        #try:
-        #    port = brds['result'][0]['port']['address']
-        #    print(f"\tPort: {port}")
-        #    fqbn = brds['result'][0]['matching_boards'][0]['fqbn']
-        #    print(f"\tFQBN: {fqbn}")
-        #except:
-        #    raise ArduinoNotConnected()
-
-        #arduino.compile(fqbn=fqbn, sketch="Arduino")
-        #arduino.upload(fqbn=fqbn, sketch="Arduino", port=port)
-
-        #self.sensor_values = dict()
-        #self.ultrasonic_minimum_distance = ultrasonic_minimum_distance
+    def __init__(self, port='/dev/ttyACM0', baudrate=19200, timeout=1):
+        # Upload arduino code
+        try:
+            arduino = pyduinocli.Arduino("./src/Arduino/arduino-cli")
+            arduino.compile(fqbn="arduino:avr:uno", sketch="./src/Arduino/Arduino.ino")
+            arduino.upload(fqbn="arduino:avr:uno", sketch="./src/Arduino/Arduino.ino", port=port)
+        except Exception as e:
+            print(e)
+            print(e.args)
 
         # Open serial port
-        port='COM3'
         try:
             self.ser = serial.Serial(port, baudrate, timeout=timeout)
             self.ser.reset_input_buffer()
         except:
             raise CouldNotOpenPort(port)
 
-        # Start serial reading thread
-        self.serial_read_thread_running = True
-        self.serial_read_thread = threading.Thread(target=self.serial_read)
-        self.serial_read_thread.start()
-
     def send_direction(self, motor_direction: MotorDirections):
         self.ser.write(motor_direction.value)
-
-    def close(self):
-        self.serial_read_thread_running = False
-
-    def update(self, sensor: Sensors, value: int):
-        self.sensor_values[sensor.value] = value
-        if (sensor.value == 7 and value == 1) or (sensor.value != 7 and value <= self.ultrasonic_minimum_distance):
-            raise SensorDistanceAlert(sensor.name)
-
-    def serial_read(self):
-        while self.serial_read_thread_running:
-            read = self.ser.read()
-            sensor_type = read[0]
-            sensor_number = read[1]
-            value = read[3]
-
-            print(f"Sensor {sensor_type} #{sensor_number}: {value}")
-
-            if sensor_type == "S":
-                match sensor_number:
-                    case "1":
-                        self.update(Sensors.ULTRASONIC_1, value)
-                    case "2":
-                        self.update(Sensors.ULTRASONIC_2, value)
-                    case "3":
-                        self.update(Sensors.ULTRASONIC_3, value)
-                    case "4":
-                        self.update(Sensors.ULTRASONIC_4, value)
-                    case "5":
-                        self.update(Sensors.ULTRASONIC_5, value)
-                    case "6":
-                        self.update(Sensors.ULTRASONIC_6, value)
-            elif sensor_type == "F":
-                self.update(Sensors.FORCE_SENSOR, value)
-
-
-        self.ser.close()
