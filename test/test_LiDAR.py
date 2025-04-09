ddfb1d033980518d2bf555453d1adf2aa01f32b4
Aleks B <ajrberezowski@gmail.com>
2025-04-08 23:21:11 -0600

    Make into class

--- test/test_LiDAR.py
+++ test/test_LiDAR.py
@@ -4,62 +4,15 @@ import sys
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR.replace(r"/test", r"/src")))
 
-from src.LiDAR.build.RunLiDAR import RunLiDAR
-from src.RaspberryPi.SharedMemory import SharedMemory
 import matplotlib.pyplot as plt
-import numpy as np
-import time
+from src.LiDAR.LiDAR import LiDAR
 
-occupancy_grid_memory = SharedMemory(shem_name="occupancy_grid", size=28462200, create=True)
+lidar = LiDAR()
+grid, origin = lidar.get_grid()
 
-RunLiDAR()
-print("LiDAR thread started")
-
-edge_buffer = 2
-while True:
-    grid = occupancy_grid_memory.read_grid()
-
-    if len(grid) > 1:
-
-        # Convolve
-        convolved_grid = [[0 for _ in grid[0]] for _ in grid]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    x_range = range(max(0, i-edge_buffer), min(len(grid), i+edge_buffer+1))
-                    y_range = range(max(0, j-edge_buffer), min(len(grid[0]), j+edge_buffer+1))
-                    for x in x_range:
-                        for y in y_range:
-                            convolved_grid[x][y] = 1
-
-        # Shrink
-        rows = len(convolved_grid)
-        cols = len(convolved_grid[0])
-        min_row = rows
-        max_row = -1
-        min_col = cols
-        max_col = -1
-        for r in range(rows):
-            for c in range(cols):
-                if convolved_grid[r][c] == 1:
-                    min_row = min(min_row, r)
-                    max_row = max(max_row, r)
-                    min_col = min(min_col, c)
-                    max_col = max(max_col, c)
-        cropped_grid = [row[min_col:max_col + 1] for row in convolved_grid[min_row:max_row + 1]]
-
-        # Clear Origin
-        orign_buffer = 4
-        origin = (len(grid[0]) // 2 - min_col, len(grid) // 2 - min_row)
-        x_range = range(max(0, origin[1] - orign_buffer), min(len(cropped_grid), origin[1] + orign_buffer + 1))
-        y_range = range(max(0, origin[0] - orign_buffer), min(len(cropped_grid[0]), origin[0] + orign_buffer + 1))
-        for x in x_range:
-            for y in y_range:
-                cropped_grid[x][y] = 0
-
-        # Plot
-        fig = plt.figure(figsize=(6, 6))
-        ax = fig.add_subplot()
-        plt.imshow(cropped_grid)
-        plt.scatter(origin[0], origin[1], color="red")
-        plt.show()
+# Plot
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot()
+plt.imshow(grid)
+plt.scatter(origin[0], origin[1], color="red")
+plt.show()
