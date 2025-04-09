d667cc21cdb6048bc2c3485730f6b7e8246a3d76
Aleks B <ajrberezowski@gmail.com>
2025-04-08 23:44:43 -0600

    Update ArduinoUno.py

--- src/Arduino/ArduinoUno.py
+++ src/Arduino/ArduinoUno.py
@@ -1,7 +1,9 @@
 import serial
 from enum import Enum
+
 from src.RaspberryPi.InternalException import CouldNotOpenPort
 import pyduinocli
+import time
 
 class MotorDirections(Enum):
     FORWARD = b"w"
@@ -12,6 +14,11 @@ class MotorDirections(Enum):
 
 class ArduinoUno:
     def __init__(self, port='/dev/ttyACM0', baudrate=19200, timeout=1):
+        self.t_accel = 0.28  # in seconds, calculated
+        self.t_const = 1.15  # in seconds, calculated
+        self.t_rotaccel = 0.28  # in seconds, calculated w/ 1 estimated value
+        self.t_rotconst = 0.77  # in seconds, calculated w/ 1 estimated value
+
         # Upload arduino code
         try:
             arduino = pyduinocli.Arduino("./src/Arduino/arduino-cli")
@@ -28,5 +35,23 @@ class ArduinoUno:
         except:
             raise CouldNotOpenPort(port)
 
-    def send_direction(self, motor_direction: MotorDirections):
-        self.ser.write(motor_direction.value)
+    def drive_one_unit(self, direction):
+        match direction:
+            case MotorDirections.FORWARD:
+                time_to_drive = self.t_accel + self.t_const
+            case MotorDirections.LEFT:
+                time_to_drive = self.t_rotaccel + self.t_rotconst
+            case MotorDirections.RIGHT:
+                time_to_drive = self.t_rotaccel + self.t_rotconst
+            case MotorDirections.BACKWARD:
+                time_to_drive = self.t_accel + self.t_const
+            case _:
+                direction = MotorDirections.STOP
+                time_to_drive = 0
+
+        self.ser.write(direction.value)
+        time.sleep(time_to_drive)
+
+    def drive_path(self, directions):
+        for direction in directions:
+            self.drive_one_unit(direction)
