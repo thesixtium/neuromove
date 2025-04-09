ccd58eed42f0939f179242ae4d70863fb5b47f5c
Aleks B <ajrberezowski@gmail.com>
2025-04-08 22:48:54 -0600

    Update test_LiDAR.py

--- test/test_LiDAR.py
+++ test/test_LiDAR.py
@@ -21,12 +21,21 @@ while True:
     grid = occupancy_grid_memory.read_grid()
 
     if len(grid) > 1:
+        convolved_grid = [[0 for _ in grid[0]] for _ in grid]
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    x_range = range(max(0, i-3), min(len(grid), i+4))
+                    y_range = range(max(0, j-3), min(len(grid[0]), j+4))
+
+                    for x in x_range:
+                        for y in y_range:
+                            convolved_grid[x][y] = 1
 
         fig = plt.figure(figsize=(6, 6))
         ax = fig.add_subplot()
         plt.imshow(grid)
-        
-        print(f"{sum([sum(i) for i in grid])} / {sum([len(i) for i in grid])}")
-
+        plt.imshow(convolved_grid, alpha=0.5)
 
         plt.show()
